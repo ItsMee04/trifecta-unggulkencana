bd1d60169eb0c9bc8100a4816a55b480b042e471
ItsMee04 <meemeeits@gmail.com>
2026-07-12 20:58:41 +0700

    Update: Update Module Jabatan

--- resources/js/modules/jabatan/components/JabatanModal.vue
+++ resources/js/modules/jabatan/components/JabatanModal.vue
@@ -35,7 +35,7 @@
                                 Nama Jabatan
                             </label>
                             <input v-model="formJabatan.jabatan" type="text" id="nama_jabatan"
-                                placeholder="e.g. Admin, Owner, Sales"
+                                placeholder="Jabatan"
                                 class="w-full px-3.5 py-2 bg-white dark:bg-slate-950 border border-slate-200 dark:border-slate-800 rounded-xl text-sm focus:outline-none focus:border-indigo-500 dark:focus:border-indigo-600 text-slate-900 dark:text-white transition" />
                             <span v-if="errors.jabatan" class="text-xs text-rose-500 font-medium block mt-1">
                                 {{ errors.jabatan }}
